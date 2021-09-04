8d3dac3f7fd1233cc441d5070dc393254ee3e597
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-09-04 11:49:53 +0530

    Delete education/Course_Shop directory

--- education/Course_Shop/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-from .models import extendeduser
-from .models import Order
-admin.site.register(extendeduser)
-admin.site.register(Order)
